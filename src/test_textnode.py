acda21be13793c7dc72c5ec3ef2778bf385f88c1
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-27 16:28:51 -0500

    add test_textnode, init,eq,repr

---
+++ src/test_textnode.py
@@ -0,0 +1,35 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def setUp(self):
+        # set all the test nodes
+        self.node = TextNode('This is a text node', TextType.BOLD)
+        self.node2 = TextNode('This is a text node', TextType.BOLD)
+        self.node3 = TextNode('This is another text node', TextType.BOLD)
+        self.node4 = TextNode('What a wonderful Italian day', TextType.ITALIC, url='rail.it')
+        self.node5 = TextNode('What a wonderful Italian day', TextType.ITALIC, url='rails.it')
+        self.node6 = TextNode('Just plain', TextType.PLAIN, url='')
+        self.node7 = TextNode('Just plain', TextType.PLAIN, '')
+
+    def test_init(self):
+        print('\nTesting initialization')
+        self.assertEqual(self.node4.text, 'What a wonderful Italian day')
+        self.assertEqual(self.node4.text_type.value, 'italic')
+        self.assertEqual(self.node4.url, 'rail.it')
+
+    def test_eq(self):
+        print('\nTesting equality method')
+        self.assertEqual(self.node, self.node2)
+        self.assertNotEqual(self.node, self.node3)
+        self.assertNotEqual(self.node4, self.node5)
+        self.assertEqual(self.node6, self.node7)
+
+    def test_repr(self):
+        print('\nTesting representation')
+        self.assertEqual(repr(self.node), f'TextNode(This is a text node, bold, {None})')
+        self.assertEqual(repr(self.node4), 'TextNode(What a wonderful Italian day, italic, rail.it)')
+
+if __name__ == '__main__':
+    unittest.main()
