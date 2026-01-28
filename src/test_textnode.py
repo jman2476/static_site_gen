690eee90f594c88730b1242c3d0ff5b85250a688
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-28 13:48:12 -0500

    resorted tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -12,21 +12,6 @@ class TestTextNode(unittest.TestCase):
         self.node6 = TextNode('Just plain', TextType.PLAIN, url='')
         self.node7 = TextNode('Just plain', TextType.PLAIN, '')
 
-        # test nodes for text_to_html:
-        self.plain = TextNode('Plain text', TextType.PLAIN)
-        self.bold = TextNode('Bold text', TextType.BOLD)
-        self.italic = TextNode('Italic text', TextType.ITALIC)
-        self.code = TextNode('Code block', TextType.CODE)
-        self.link = TextNode(
-            'Link text', 
-            TextType.LINK, 
-            'https://google.com')
-        self.image = TextNode(
-            'Alt image text',
-            TextType.IMAGE,
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/2560px-Patates.jpg'
-        )
-        self.none = TextNode('None type', None)
 
     def test_init(self):
         print('\nTextNode initialization')
@@ -46,23 +31,51 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(repr(self.node), f'TextNode(This is a text node, bold, {None})')
         self.assertEqual(repr(self.node4), 'TextNode(What a wonderful Italian day, italic, rail.it)')
 
-    def test_text_to_html(self):
-        print('\nTextNode text_to_html_node')
-        with self.assertRaises(
-            ValueError, msg='Text type not in TextType enum'):
-            text_node_to_html_node(self.none)
+
+class TestTextToHTMLNode(unittest.TestCase):
+    def setUp(self):
+        # test nodes for text_to_html:
+        self.plain = TextNode('Plain text', TextType.PLAIN)
+        self.bold = TextNode('Bold text', TextType.BOLD)
+        self.italic = TextNode('Italic text', TextType.ITALIC)
+        self.code = TextNode('Code block', TextType.CODE)
+        self.link = TextNode(
+            'Link text', 
+            TextType.LINK, 
+            'https://google.com')
+        self.image = TextNode(
+            'Alt image text',
+            TextType.IMAGE,
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/2560px-Patates.jpg'
+        )
+        self.none = TextNode('None type', None)
+
+    def test_plain_to_html(self):
+        print('\nTextToHTML plain')
         plain_node = text_node_to_html_node(self.plain)
         self.assertEqual(plain_node.tag, None)
         self.assertEqual(plain_node.value, 'Plain text')
+
+    def test_bold_to_html(self):
+        print('\nTextToHTML bold')
         bold_node = text_node_to_html_node(self.bold)
         self.assertEqual(bold_node.tag, 'b')
         self.assertEqual(bold_node.value, 'Bold text')
+
+    def test_italic_to_html(self):
+        print('\nTextToHTML italic')
         italic_node = text_node_to_html_node(self.italic)
         self.assertEqual(italic_node.tag, 'i')
         self.assertEqual(italic_node.value, 'Italic text')
+
+    def test_code_to_html(self):
+        print('\nTextToHTML code')
         code_node = text_node_to_html_node(self.code)
         self.assertEqual(code_node.tag, 'code')
         self.assertEqual(code_node.value, 'Code block')
+
+    def test_link_to_html(self):
+        print('\nTextToHTML link')
         link_node = text_node_to_html_node(self.link)
         self.assertEqual(link_node.tag, 'a')
         self.assertEqual(
@@ -70,6 +83,9 @@ class TestTextNode(unittest.TestCase):
             'Link text',
             {'href': 'https://google.com'}
             )
+        
+    def test_img_to_html(self):
+        print('\nTextToHTML image')
         image_node = text_node_to_html_node(self.image)
         self.assertEqual(image_node.tag, 'img')
         self.assertEqual(image_node.value, None)
@@ -79,7 +95,13 @@ class TestTextNode(unittest.TestCase):
                 'alt': 'Alt image text',
                 'src': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/2560px-Patates.jpg'
             })
-
-
+        
+    def test_none_to_html(self):
+        print('\nTextToHTML none')
+        with self.assertRaises(
+            ValueError, msg='Text type not in TextType enum'):
+            text_node_to_html_node(self.none)
+    
+      
 if __name__ == '__main__':
     unittest.main()
