fbc8295ef0b520f44a93c9334b5b2775883a7015
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-28 10:10:39 -0500

    add leafnode, no tests yet

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,40 @@ class HTMLNode():
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super(self, tag, value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            if self.__check_empty():
+                return f'<{self.tag}>'
+            else:
+                raise ValueError('Tag empty. Must have value or be valid empty tag')
+        if self.tag is None:
+            return f'{self.value}'
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+        
+    def __check_empty(self):
+        empty_tags = [
+                'area',
+                'base',
+                'br',
+                'col',
+                'embed',
+                'hr',
+                'img',
+                'input',
+                'link',
+                'meta',
+                'param',
+                'source',
+                'track',
+                'wbr'
+                ]
+        return self.tag in empty_tags
+
+
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'
