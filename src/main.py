29bd8f1fa7fa7fdf8b23e4dd4b9a941813b8bfd7
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-30 13:26:51 -0500

    add recursive page generation

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from copy_static import delete_public_dir, create_empty_public_dir, copy_directory_contents
-from page_gen import generate_page
+from page_gen import generate_page_recursive
 
 
 def main():
     # create_empty_public_dir()
     copy_directory_contents('./static', './public')
-    generate_page('content/index.md', 'template.html', 'public/index.html')
+    generate_page_recursive('content', 'template.html', 'public')
 
 
 
--- src/page_gen.py
+++ src/page_gen.py
@@ -29,3 +29,16 @@ def generate_page(from_path, template_path, dest_path):
         file.write(html)
 
     print('Generation done')
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    content_dir = os.listdir(dir_path_content)
+    for entry in content_dir:
+        entry_path = os.path.join(dir_path_content, entry)
+        if os.path.isfile(entry_path):
+            dest_path = os.path.join(dest_dir_path, 'index.html')
+            generate_page(entry_path, template_path, dest_path)
+        else:
+            dest_dir = os.path.join(dest_dir_path, entry)
+            if not os.path.exists(dest_dir_path):
+                os.mkdir(dest_dir_path)
+            generate_page_recursive(entry_path, template_path, dest_dir)
