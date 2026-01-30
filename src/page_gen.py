78af571cc8ff834518a7fd2512b2391b5998f93c
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-30 00:52:53 -0500

    add static content

---
+++ src/page_gen.py
@@ -0,0 +1,6 @@
+def copy_directory_contents(source, destination):
+    #check that source, destination are valid paths
+
+    #if directory, identify sub-directories and files
+    ## copy files to destination
+    ## call copy_directory_contents on sub_directories
